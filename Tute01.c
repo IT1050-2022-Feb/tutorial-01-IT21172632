0a56b77c734b245a1ee50d8b8697d3ef930b8f2c
Sandani <99640030+IT21172632@users.noreply.github.com>
2022-02-14 08:38:09 +0000

    Tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+    int i;
+    float mark, sum=0, avg;
+    printf("Enter the marks of first subject: ");
+    printf("Enter the marks of second subject: ");
+    for(i=0; i<5; i++)
+    {
+        scanf("%f", &mark);
+        sum = sum+mark;
+    }
+    avg = sum/5;
+    printf("\nAverage Mark = %0.2f", avg);
+    getch();
+    
   
   return 0;
 }
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,15 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int n,sum=0;
+  printf("Enter the value of n:");
+  scanf("%d",&n);
   
+  for(int i=1;i<=n;i++){
+    sum+=i;
+  }
+  printf("n -> %d\nSum = %d\n", n, sum);
   return 0;
 }
+
 
